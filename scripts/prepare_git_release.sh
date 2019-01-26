c976ffa4504f4a385f7bcc15258bf996ebbd1357
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-01-27 01:36:47 +1100

    oops

--- scripts/prepare_git_release.sh
+++ scripts/prepare_git_release.sh
@@ -5,7 +5,7 @@ if [ -n "$TRAVIS_BRANCH" ]; then
   # If so, prepare thigns so taht its possible to push to the branch
   git checkout "$TRAVIS_BRANCH";
   test $(git rev-list HEAD --max-count=1) == "$TRAVIS_COMMIT"
-  openssl aes-256-cbc -K $encrypted_64b59bdd3d91_key -iv$encrypted_64b59bdd3d91_iv -in github_deploy_key.enc -out github_deploy_key-d
+  openssl aes-256-cbc -K $encrypted_64b59bdd3d91_key -iv $encrypted_64b59bdd3d91_iv -in github_deploy_key.enc -out github_deploy_key -d
   chmod 600 github_deploy_key
   eval $(ssh-agent -s)
   ssh-add github_deploy_key

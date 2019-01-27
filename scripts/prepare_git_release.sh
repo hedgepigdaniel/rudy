b6935dee3c7e9007244d9e4471f8d706690d9a4e
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-01-28 10:17:42 +1100

    Correct check for pr vs push build

--- scripts/prepare_git_release.sh
+++ scripts/prepare_git_release.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-# Test if we are running on a push build (not a PR)
-if [ -n "$TRAVIS_BRANCH" ]; then
-  # If so, prepare thigns so taht its possible to push to the branch
+# Test if we are running on a push build (not a PR) and prepare for a release
+if [ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+
+  # By default travis uses deteached HEAD - switch to the branch itself
   git checkout "$TRAVIS_BRANCH";
+
+  # Assert that the branch HEAD is the intended commit for this job
   test $(git rev-list HEAD --max-count=1) == "$TRAVIS_COMMIT"
+
+  # Decrypt the github deploy key and set it as the default key
   openssl aes-256-cbc -K $encrypted_64b59bdd3d91_key -iv $encrypted_64b59bdd3d91_iv -in github_deploy_key.enc -out github_deploy_key -d
   chmod 600 github_deploy_key
   eval $(ssh-agent -s)
   ssh-add github_deploy_key
+
+  # Set the git remtoe url to use SSH so that the deploy key is usable
   git remote set-url origin "git@github.com:${TRAVIS_REPO_SLUG}.git"
+
+  # Put the NPM token in .npmrc so that it is usable
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 fi;

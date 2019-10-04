966e2255223f12b9744b5d4906ae1b79712b335e
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-10-04 21:29:24 +1000

    0.0.1 (125383aa426d8dd47c013eb1b4cc1fe685bfece4)

---
+++ es/utils/handlePress.js
@@ -0,0 +1,30 @@
+import { redirect } from '@respond-framework/rudy';
+export default (function (action, routes, shouldDispatch, dispatch, onClick, target, isRedirect, fullUrl, history, e) {
+  var prevented = e.defaultPrevented;
+  var notModified = !isModified(e);
+  var shouldGo = true;
+
+  if (onClick) {
+    shouldGo = onClick(e) !== false; // onClick can return false to prevent dispatch
+  }
+
+  if (!target && e && e.preventDefault && notModified) {
+    e.preventDefault();
+  }
+
+  if (action && shouldGo && shouldDispatch && !target && !prevented && notModified && e.button === 0) {
+    action = isRedirect ? redirect(action) : action;
+    return dispatch(action);
+  }
+
+  if (!action && !target && fullUrl.indexOf('http') === 0) {
+    window.location.href = fullUrl;
+  }
+
+  return undefined;
+});
+
+var isModified = function isModified(e) {
+  return !!(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey);
+};
+//# sourceMappingURL=handlePress.js.map

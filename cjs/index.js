3c7ae5b5dc4132460076687fa057ec54e5ade880
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-10-04 20:14:21 +1000

    0.0.1 (2a82c546784bbb85eb8edbbe6b95a1d160d2d7aa)

---
+++ cjs/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "isServer", {
+  enumerable: true,
+  get: function get() {
+    return _isServer.default;
+  }
+});
+Object.defineProperty(exports, "createSelector", {
+  enumerable: true,
+  get: function get() {
+    return _createSelector.default;
+  }
+});
+
+var _isServer = _interopRequireDefault(require("./isServer"));
+
+var _createSelector = _interopRequireDefault(require("./createSelector"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=index.js.map
---
+++ cjs/isServer.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+/* eslint-env browser */
+var _default = function _default() {
+  return !(typeof window !== 'undefined' && window.document && window.document.createElement);
+};
+
+exports.default = _default;
+//# sourceMappingURL=isServer.js.map

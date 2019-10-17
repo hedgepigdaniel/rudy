4f0e27dfea01b7bbb3fae9b4580a8af68af45855
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-10-17 18:20:04 +1100

    0.0.1 (f0676c816aefe001878b93d479b10d08a58a5f48)

---
+++ es/isServer.js
@@ -0,0 +1,5 @@
+/* eslint-env browser */
+export default (function () {
+  return !(typeof window !== 'undefined' && window.document && window.document.createElement);
+});
+//# sourceMappingURL=isServer.js.map
---
+++ ts/createSelector.d.ts
@@ -0,0 +1,3 @@
+declare const _default: (name: string, selector?: string | Function | undefined) => any;
+export default _default;
+//# sourceMappingURL=createSelector.d.ts.map

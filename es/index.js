1cbfdb2de975761da7d57a73ffbbef4d103375d6
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-10-17 18:17:58 +1100

    0.0.1 (f0676c816aefe001878b93d479b10d08a58a5f48)

---
+++ es/index.js
@@ -0,0 +1,56 @@
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+import { isServer as defaultIsServer, createSelector as defaultCreateSelector } from '@respond-framework/utils';
+export default (function (options) {
+  var _ref = options || {},
+      keyOrSelector = _ref.title,
+      _ref$isServer = _ref.isServer,
+      isServer = _ref$isServer === void 0 ? defaultIsServer : _ref$isServer,
+      _ref$createSelector = _ref.createSelector,
+      createSelector = _ref$createSelector === void 0 ? defaultCreateSelector : _ref$createSelector,
+      _ref$setTitle = _ref.setTitle,
+      setTitle = _ref$setTitle === void 0 ? function (title) {
+    // eslint-disable-next-line no-undef
+    window.document.title = title;
+  } : _ref$setTitle;
+
+  var selectTitleState = createSelector('title', keyOrSelector);
+  return function (api) {
+    return (
+      /*#__PURE__*/
+      function () {
+        var _ref2 = _asyncToGenerator(
+        /*#__PURE__*/
+        regeneratorRuntime.mark(function _callee(req, next) {
+          var title;
+          return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+              switch (_context.prev = _context.next) {
+                case 0:
+                  title = selectTitleState(api.getState());
+
+                  if (!isServer() && typeof title === 'string') {
+                    // eslint-disable-next-line no-undef
+                    setTitle(title);
+                  }
+
+                  return _context.abrupt("return", next());
+
+                case 3:
+                case "end":
+                  return _context.stop();
+              }
+            }
+          }, _callee);
+        }));
+
+        return function (_x, _x2) {
+          return _ref2.apply(this, arguments);
+        };
+      }()
+    );
+  };
+});
+//# sourceMappingURL=index.js.map

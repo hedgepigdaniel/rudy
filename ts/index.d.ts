4c423ca3f4ad79d7fe3a3364c111c1b897412333
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-03-25 19:33:02 +1100

    0.0.1 (e9b6dc1ff4dac25edcccc4f0b883005e8180c120)

---
+++ ts/index.d.ts
@@ -0,0 +1,26 @@
+import { ScrollTarget } from 'scroll-behavior';
+import { Api, Middleware, LocationEntry, FluxStandardRoutingAction, Request, ScrollRestorer, ScrollRestorerCreator } from '@respond-framework/types';
+export { ScrollPosition } from 'scroll-behavior';
+export declare type ShouldUpdateScroll<Action extends FluxStandardRoutingAction> = (request: Request<Action>) => ScrollTarget;
+export declare type RestoreScrollOptions<Action extends FluxStandardRoutingAction> = {
+    shouldUpdateScroll?: ShouldUpdateScroll<Action>;
+};
+export declare class RudyScrollRestorer<Action extends FluxStandardRoutingAction> implements ScrollRestorer<Action> {
+    private options;
+    private behavior;
+    private lastRequest?;
+    private api;
+    private transitionHooks;
+    private nextHookIndex;
+    private makeStorageKey;
+    private saveScrollPosition;
+    readScrollPosition: (entry: LocationEntry<Action>, key: string | null) => [number, number] | null;
+    constructor(api: Api<Action>, options?: RestoreScrollOptions<Action>);
+    private getCurrentLocation;
+    saveScroll: Middleware<Action>;
+    restoreScroll: Middleware<Action>;
+    updateScroll: () => void;
+}
+declare const _default: <Action extends FluxStandardRoutingAction>(options?: RestoreScrollOptions<Action> | undefined) => ScrollRestorerCreator<Action>;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
